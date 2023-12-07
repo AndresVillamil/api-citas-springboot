aa3a40d95151b86a474478bc693f4a9e0dc2cd14
AndresVillamil <90357078+AndresVillamil@users.noreply.github.com>
2023-12-06 21:51:52 -0500

    Entidades

---
+++ src/main/java/com/citas/apicitas/entities/Doctor.java
@@ -0,0 +1,35 @@
+package com.citas.apicitas.entities;
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name="doctor")
+
+public class Doctor {
+    
+    @Id
+    @Column(name="id_profesional")
+    private long idProfesional;
+
+    @Column
+    private String name;
+
+    @Column 
+    private String apellido;
+
+    @Column
+    private String correo;
+
+    @Enumerated(EnumType.STRING)
+    @Column
+    private Especialidad especialidad;
+
+    @OneToMany(mappedBy = "doctor")
+    private Set<Cita> citas;
+
+    public enum Especialidad{medicina_interna, medinica_general}
+
+}
--- src/main/java/com/citas/apicitas/entities/Paciente.java
+++ src/main/java/com/citas/apicitas/entities/Paciente.java
@@ -1,5 +1,35 @@
 package com.citas.apicitas.entities;
 
+import java.time.LocalDateTime;
+import java.util.Set;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name="paciente")
+
 public class Paciente {
-    
+    @Id
+    @Column(name="id_numero_cedula")
+    private long idNumeroCedula;
+
+    @Column
+    private String nombre;
+
+    @Column
+    private String apellido;
+
+    @Column
+    private String telefono;
+
+    @Column(name="fecha_nacimiento")
+    private LocalDateTime fechaNacimiento;
+
+    @OneToMany(mappedBy="paciente")
+    private Set<Cita> citas;
 }
